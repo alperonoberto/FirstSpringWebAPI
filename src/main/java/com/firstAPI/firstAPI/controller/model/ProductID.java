5e8743a29c734407025dca906850e63680f2e1f3
alperonoberto <alperonoberto@gmail.com>
2023-09-07 12:14:12 +0200

    feat: initial commit

---
+++ src/main/java/com/firstAPI/firstAPI/controller/model/ProductID.java
@@ -0,0 +1,7 @@
+package com.firstAPI.firstAPI.controller.model;
+
+public class ProductID {
+    private final String id;
+    public ProductID(final String id) { this.id = id; }
+    public String getId() { return this.id; }
+}
